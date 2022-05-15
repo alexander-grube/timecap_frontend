5df0a4238edfcca836ec669aa51b711f7f4b283a
Alexander Grube <alexander.grube@cancom.de>
2022-05-15 14:22:55 +0200

    add router and error page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
 import './App.css'
 import { Login } from './components/Login'
+import Error from './components/Error'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
 
 
   return (
-    <Login />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/components/Error.tsx
@@ -0,0 +1,9 @@
+import '../App.css'
+
+function Error() {
+  return (
+      <h1 className="center">Error - Page Not Found</h1>
+  )
+}
+
+export default Error
